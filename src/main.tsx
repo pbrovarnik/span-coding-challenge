9fd88b4d90c12665c91cc7cd8b4d2fb9daae3dd1
Pasha <pasha@getinkworks.com>
2024-08-06 21:12:11 -0700

    clean code and styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import App from './second-question.tsx';
+import App from './after-thought-attempt';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
